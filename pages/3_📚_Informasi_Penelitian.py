33f8b1f18bb5170b5ea3679952629a2a49fd0def
zeafiola-ops <zeafiola@gmail.com>
2026-07-05 22:55:58 +0700

    Update 3_📚_Informasi_Penelitian.py

--- pages/3_📚_Informasi_Penelitian.py
+++ pages/3_📚_Informasi_Penelitian.py
@@ -177,7 +177,7 @@ with col1:
 
     st.image(
         str(LOGO_DASHBOARD),
-        width=145
+        width=130
     )
 
 with col2:
@@ -208,7 +208,7 @@ with col3:
 
     st.image(
         str(LOGO_DARMAJAYA),
-        width=145
+        width=160
     )
 
 st.markdown("</div>", unsafe_allow_html=True)
